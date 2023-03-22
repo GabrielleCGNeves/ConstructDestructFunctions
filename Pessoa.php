17b7dfe32675e3cb995a0cb4c542eb559e102a5d
GabrielleCGNeves <gabriellecarvallhoneves@gmail.com>
2023-03-22 07:45:17 -0300

    :tada: created class Pessoa

---
+++ Pessoa.php
@@ -0,0 +1,11 @@
+<?php
+    class Pessoa {
+        var $codigo;
+        var $nome;
+        var $altura;
+        var $idade;
+        var $nascimento;
+        var $escolaridade;
+        var $salario;
+    }
+?>
