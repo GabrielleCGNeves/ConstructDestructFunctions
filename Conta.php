69e942827d345ea38bf0b21e1e7c640468da10d5
GabrielleCGNeves <gabriellecarvallhoneves@gmail.com>
2023-03-22 07:52:15 -0300

    :sparkles: added new functions

--- Conta.php
+++ Conta.php
@@ -7,5 +7,21 @@
         var $senha;
         var $saldo;
         var $cancelada;
+
+        function retirar($quantia){
+            if($quantia > 0){
+                $this -> saldo -= $quantia;
+            }
+        }
+    
+        function depositar($quantia){
+            if($quantia > 0){
+                $this -> saldo += $quantia;
+            }
+        }
+    
+        function obterSaldo(){
+            return $this -> saldo;
+        }
     }
 ?>
