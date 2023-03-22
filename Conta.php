5c42ec04c43d1cffa6f65ca784cae4247375d787
GabrielleCGNeves <gabriellecarvallhoneves@gmail.com>
2023-03-22 07:51:29 -0300

    :tada: created class Conta

---
+++ Conta.php
@@ -0,0 +1,11 @@
+<?php
+    class Conta{
+        var $agencia;
+        var $codigo;
+        var $dataDeCriacao;
+        var $titular;
+        var $senha;
+        var $saldo;
+        var $cancelada;
+    }
+?>
