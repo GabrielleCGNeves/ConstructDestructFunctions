e6636596661f51160306b7a40276cc76ef7609ee
GabrielleCGNeves <gabriellecarvallhoneves@gmail.com>
2023-03-23 19:34:52 -0300

    :bug: fixed instances

--- Objeto.php
+++ Objeto.php
@@ -2,40 +2,40 @@
     include_once 'Pessoa.php';
     include_once 'Conta.php';
 
-    $eduardo = new Pessoa;
-    $eduardo -> Codigo = 10;
-    $eduardo -> Nome = "Eduardo Soares";
-    $eduardo -> Altura = 1.72;
-    $eduardo -> Idade = 23;
-    $eduardo -> Nascimento = '10.01.2000';
-    $eduardo -> Escolatidade = 'Ensino Superior';
+    $eduardo = new Pessoa();
+    $eduardo -> codigo = 10;
+    $eduardo -> nome = "Eduardo Soares";
+    $eduardo -> altura = 1.72;
+    $eduardo -> idade = 23;
+    $eduardo -> nascimento = '10.01.2000';
+    $eduardo -> escolaridade = 'Ensino Superior';
 
-    echo "Manipuolando o objeto $eduardo -> Nome: \n";
-    echo "{$eduardo -> Nome} é formado em: {$eduardo -> Escolaridade}\n";
+    echo "Manipuolando o objeto $eduardo -> nome: \n";
+    echo "{$eduardo -> nome} é formado em: {$eduardo -> escolaridade}\n";
     
     $eduardo -> Formar('Tecnico em Eletricidade');
-    echo "{$eduardo -> Nome} é formado em: {$eduardo -> Escolaridade}\n";
-    echo "{$eduardo -> Nome} possui {$eduardo -> Idade} anos \n";
+    echo "{$eduardo -> nome} é formado em: {$eduardo -> escolaridade}\n";
+    echo "{$eduardo -> nome} possui {$eduardo -> idade} anos \n";
 
     $eduardo -> Envelhecer(1);
-    echo "{$eduardo -> Nome} possui {$eduardo -> Idade} anos \n"; 
+    echo "{$eduardo -> nome} possui {$eduardo -> idade} anos \n"; 
 
-    $conta_eduardo = new Conta;
-    $conta_eduardo ->Agencia = 6677;
-    $conta_eduardo ->Codigo = "CC.12 34. 56";
-    $conta_eduardo ->DatadeCriacao = "10/07/02";
-    $conta_eduardo ->Titular = $eduardo;
-    $conta_eduardo ->Senha = 9876;
-    $conta_eduardo ->Saldo = 567.89;
-    $conta_eduardo ->Cancelada = false;
+    $conta_eduardo = new Conta($agencia, $codigo, $dataDeCriacao, $titular, $senha, $saldo);
+    $conta_eduardo -> agencia = 6677;
+    $conta_eduardo -> codigo = "CC.12 34. 56";
+    $conta_eduardo -> datadeCriacao = "10/07/02";
+    $conta_eduardo -> titular = $eduardo;
+    $conta_eduardo -> senha = 9876;
+    $conta_eduardo -> saldo = 567.89;
+    $conta_eduardo -> cancelada = false;
 
     echo "<br>";
-    echo "O saldo atual é de R\$ {$conta_eduardo->obterSaldo()} <br>";
-    echo "Manipulando a conta de: {$conta_eduardo ->Titular->Nome} <br>";
+    echo "O saldo atual é de R\$ {$conta_eduardo -> obterSaldo()} <br>";
+    echo "Manipulando a conta de: {$conta_eduardo -> titular->nome} <br>";
 
-    $conta_eduardo->Depositar(20);
-    echo "O saldo atual é de R\${$conta_eduardo->obterSaldo()} <br>";
+    $conta_eduardo -> Depositar(20);
+    echo "O saldo atual é de R\${$conta_eduardo -> obterSaldo()} <br>";
 
-    $conta_eduardo->Retirar(10);
-    echo "O saldo atual é de R\${$conta_eduardo->obterSaldo()} <br>";
+    $conta_eduardo -> Retirar(10);
+    echo "O saldo atual é de R\${$conta_eduardo -> obterSaldo()} <br>";
 ?>
