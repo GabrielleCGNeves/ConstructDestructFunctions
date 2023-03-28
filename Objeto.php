e3c9a041e9e762545d0e77d07a36478c30be8b20
GabrielleCGNeves <gabriellecarvalhoneves@gmail.com>
2023-03-27 22:01:49 -0300

    :adhesive_bandage: fixed grandes errors

--- Objeto.php
+++ Objeto.php
@@ -2,35 +2,30 @@
     include_once 'AbstractsClasses/Pessoa.php';
     include_once 'AbstractsClasses/Conta.php';
 
-    $eduardo = new Pessoa($codigo, $nome, $altura, $idade, $nascimento, $escolaridade, $salario);
-    $eduardo -> codigo = 10;
-    $eduardo -> nome = "Eduardo Soares";
-    $eduardo -> altura = 1.72;
-    $eduardo -> idade = 23;
-    $eduardo -> nascimento = '10.01.2000';
-    $eduardo -> escolaridade = 'Ensino Superior';
+    $eduardo = new Pessoa(10, "Eduardo Soares", 1.72, 23, '10.01.2000', 'Ensino Superior', 5000);
 
-    echo "Manipuolando o objeto $eduardo -> nome: \n";
-    echo "{$eduardo -> nome} é formado em: {$eduardo -> escolaridade}\n";
+    echo "Manipulando o objeto {$eduardo -> nome} <br>";
+    echo "{$eduardo -> nome} é formado em: {$eduardo -> escolaridade}<br>";
     
     $eduardo -> Formar('Tecnico em Eletricidade');
-    echo "{$eduardo -> nome} é formado em: {$eduardo -> escolaridade}\n";
-    echo "{$eduardo -> nome} possui {$eduardo -> idade} anos \n";
+    echo "{$eduardo -> nome} é formado em: {$eduardo -> escolaridade}<br>";
+    echo "{$eduardo -> nome} possui {$eduardo -> idade} anos <br>";
 
     $eduardo -> Envelhecer(1);
-    echo "{$eduardo -> nome} possui {$eduardo -> idade} anos \n"; 
+    echo "{$eduardo -> nome} possui {$eduardo -> idade} anos <br>"; 
 
-    $conta_eduardo = new Conta($agencia, $codigo, $dataDeCriacao, $titular, $senha, $saldo);
+    $conta_eduardo = new Conta(6677, "CC.12 34. 56", $eduardo, "10.05.2021", 9876, 567.89);
     $conta_eduardo -> agencia = 6677;
     $conta_eduardo -> codigo = "CC.12 34. 56";
-    $conta_eduardo -> datadeCriacao = "10/07/02";
     $conta_eduardo -> titular = $eduardo;
+    $conta_eduardo -> dataDeCriacao = "10.05.2021";
     $conta_eduardo -> senha = 9876;
     $conta_eduardo -> saldo = 567.89;
     $conta_eduardo -> cancelada = false;
 
     echo "<br>";
     echo "O saldo atual é de R\$ {$conta_eduardo -> obterSaldo()} <br>";
+
     echo "Manipulando a conta de: {$conta_eduardo -> titular->nome} <br>";
 
     $conta_eduardo -> Depositar(20);
